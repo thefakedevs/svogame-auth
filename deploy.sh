80bd04245819a97d2421472dc5928eb357983580
lavafrai <lavafrai@yandex.ru>
2026-04-07 01:18:41 +0300

    feat: cicd deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+exec 9>/tmp/svogame-auth-deploy.lock
+flock -n 9 || {
+  echo "Another deployment is already running"
+  exit 1
+}
+
+git fetch origin production
+git reset --hard origin/production
+
+docker compose pull
+docker compose up -d --build --remove-orphans
